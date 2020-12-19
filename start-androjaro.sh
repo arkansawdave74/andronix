ca1fa32af619225adf2bf8b902c1e8f548e430df
arkansawdave74 <16846818+arkansawdave74@users.noreply.github.com>
2020-12-19 04:25:26 -0600

    Add files via upload
    Adds access to internal storage

---
+++ start-androjaro.sh
@@ -0,0 +1,32 @@
+cd $(dirname $0)
+pulseaudio -k >>/dev/null 2>&1
+pulseaudio --start >>/dev/null 2>&1
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r androjaro-fs"
+if [ -n "$(ls -A androjaro-binds)" ]; then
+for f in androjaro-binds/* ;do
+. $f
+done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b androjaro-fs/root:/dev/shm"
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=$TERM"
+command+=" LANG=en_US.UTF-8"
+command+=" LC_ALL=C"
+command+=" LANGUAGE=en_US"
+command+=" /bin/bash --login"
+com="$@"
+if [ -z "$1" ];then
+exec $command
+else
+$command -c "$com"
+fi
